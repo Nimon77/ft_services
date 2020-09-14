1cff281f5487e76a3769c9ca52167062165ddba0
nsimon <nsimon@student.42.fr>
2020-09-14 15:29:41 +0200

    metallb work

--- start.sh
+++ start.sh
@@ -33,11 +33,21 @@ kubectl apply -f metallb/metallb-config.yaml
 minikube ip
 
 echo -e "\033[47m\033[32m nginx image build \033[m"
-docker build -t nginx:42 nginx/ > /dev/null
-#docker build -t grafana:42 grafana/
-#docker build -t wordpress:42 wordpress/
+docker build -t nginx:42 nginx/
+echo -e "\033[47m\033[32m influxdb image build \033[m"
+docker build -t influxdb:42 influxdb/
+echo -e "\033[47m\033[32m grafana image build \033[m"
+docker build -t grafana:42 grafana/
+echo -e "\033[47m\033[32m wordpress image build \033[m"
+docker build -t wordpress:42 wordpress/
 
-echo -e "\033[47m\033[32m nginx deployment 생성 \033[m"
+kubectl create configmap default-nginx-config --from-file=nginx/website.conf
+
+echo -e "\033[47m\033[32m nginx deployment \033[m"
 kubectl apply -f nginx-deployment.yaml
-#kubectl apply -f grafana-deployment.yaml
-#kubectl apply -f wordpress-deployment.yaml
+echo -e "\033[47m\033[32m influxdb deployment \033[m"
+kubectl apply -f influxdb-deployment.yaml
+echo -e "\033[47m\033[32m grafana deployment \033[m"
+kubectl apply -f grafana-deployment.yaml
+echo -e "\033[47m\033[32m wordpress deployment \033[m"
+kubectl apply -f wordpress-deployment.yaml
